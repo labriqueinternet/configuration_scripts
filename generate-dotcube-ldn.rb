5b175d985bec0769bcd50cd7a282f30d7aa253a4
Sebastien Badia <seb@sebian.fr>
2015-11-20 01:13:44 +0100

    ldn: Add the little script for ldn dotcube

---
+++ generate-dotcube-ldn.rb
@@ -0,0 +1,48 @@
+#!/usr/bin/env ruby
+#
+# Copyright (C) 2015 LaBriqueInternet <discussions@listes.labriqueinter.net>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; version 2 of
+# the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+# http://internetcu.be/dotcubefiles.html
+# https://wiki.ldn-fai.net/wiki/Ajouter_un_compte_VPN
+
+require 'json'
+require 'yaml'
+
+if ARGV.length != 1
+  puts "generate-dotcube.rb LOGIN"
+  exit 1
+end
+
+user = ARGV[0]
+vpn = {}
+common = JSON.load(File.read('/srv/dotcube/dotcube-common.json'))
+yaml = YAML.load_file('/srv/puppet/production/puppet/hiera/ldn.yaml')
+
+if ! File.exist?("/srv/ca-openvpn-clients/keys/#{user}.crt")
+  puts "user #{user} seems not present..."
+  exit 1
+end
+
+vpn['ip6_net'] = yaml['openvpn::users'][user]['prefix']
+vpn['crt_server_ca'] = File.read('/root/ca_server.crt')
+vpn['crt_client'] = File.read("/srv/ca-openvpn-clients/keys/#{user}.crt")
+vpn['crt_client_key'] = File.read("/srv/ca-openvpn-clients/keys/#{user}.key")
+
+File.open("/srv/dotcube/#{user}.json","w") do |f|
+  f.write(JSON.pretty_generate(common.merge(vpn)))
+end
+puts "dotcube file generated /srv/dotcube/#{user}.json"
